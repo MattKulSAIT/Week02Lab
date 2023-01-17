cd17e7dfcb3cb542e23b391f8cec33f44069a780
mdkul <mdkul@MSI>
2023-01-17 11:22:06 -0700

    Lab 2 Part 4 Git Hub stuff This is a Description.

---
+++ src/helloworldlab2/HelloWorldLab2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldlab2;
+
+/**
+ *
+ * @author mdkul
+ */
+public class HelloWorldLab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("This is the Hello World code for part 4 of lab 2");
+    }
+    
+}
